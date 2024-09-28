d8206490452d3ca28760253f039ef6fa7314f18e
Sky Summer <skysommer0317@gmail.com>
2024-09-28 04:04:06 +0100

    add theme function and todo.done style

--- front/app.js
+++ front/app.js
@@ -1,16 +1,19 @@
 const todoInput = document.getElementById('todo-input');
 const todoBtn = document.getElementById('todo-btn');
 const todoList = document.getElementById('todo-list');
+const themeBtn = document.getElementById('theme-toggle');
 
 todoBtn.addEventListener('click', addTodo);
+themeBtn.addEventListener('click', toggleTheme);
 
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let isDark = JSON.parse(localStorage.getItem('isDark')) || false;
 
 function renderTodos() {
     todoList.innerHTML = '';
     todos.forEach((todo) => {
       const todoItem = document.createElement('li');
-      todoItem.className = 'todo-item';
+      todoItem.className = `todo-item ${todo.done ? 'done' : ''}`;
       todoItem.dataset.id = todo.id;
       todoItem.innerHTML = `
         <input type="checkbox" class="checkbox" ${todo.done ? 'checked' : ''}>
@@ -19,7 +22,9 @@ function renderTodos() {
       `;
 
       const checkbox = todoItem.querySelector('.checkbox');
-      checkbox.addEventListener('change', () => toggleTodo(todo.id));
+      checkbox.addEventListener('change', () => {
+        toggleTodo(todo.id);
+      });
 
       const deleteBtn = todoItem.querySelector('.delete-btn');
       deleteBtn.addEventListener('click', () => deleteTodo(todo.id));
@@ -56,12 +61,29 @@ function toggleTodo(id) {
   const todo = todos.find(todo => todo.id === id);
   if (todo) {
     todo.done = !todo.done;
+    const todoItem = document.querySelector(`.todo-item[data-id="${id}"]`);
+    if (todoItem) {
+      todoItem.classList.toggle('done', todo.done);
+    }
   }
-  renderTodos();
+  saveTodos();
 }
 
 function saveTodos() {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
+function toggleTheme() {
+  isDark = !isDark;
+  document.body.classList.toggle('dark-theme', isDark);
+  themeBtn.innerHTML = isDark ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+  localStorage.setItem('isDark', isDark);
+}
+
+function initTheme() {
+  document.body.classList.toggle('dark-theme', isDark);
+  themeBtn.innerHTML = isDark ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+}
+
 renderTodos();
+initTheme();
